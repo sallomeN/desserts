dab56e2489da46b0890786c1061fc233788f7105
sallomeN <nozadzesalome763@gmail.com>
2024-12-21 11:42:48 +0400

    adding functions to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,50 @@ import Cart from "./components/Cart";
 import "./styles/App.css";
 
 function App() {
+  const [cart, setCart] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalQuantity, setTotalQuantity] = useState(0);
+
+  const updateTotalPrice = (updatedCart) => {
+  const totalPrice = updatedCart.reduce((totalCost, cartItem) => {
+    return totalCost + cartItem.price * cartItem.quantity
+  }, 0);
+    
+    setTotalPrice(totalPrice)
+  }
+
+  const updateTotalQuantity = (updatedCart) => {
+  const newTotalQuantity = updatedCart.reduce(
+    (count, item) => count + item.quantity,
+    0
+  );
+  setTotalQuantity(newTotalQuantity)
+  }
+
+  const addToCart = (dessert) => {
+    const updatedCart = [...cart]
+    const existingItem = updatedCart.find((item) => item.name === dessert.name)
+
+    if (existingItem === true) {
+      existingItem.quantity += 1
+    } else {
+      updatedCart.push({ ...dessert, quantity: 1 })
+    }
+
+    setCart(updatedCart)
+    updateTotalPrice(updatedCart)
+    updateTotalQuantity(updatedCart)
+  }
 
 
+  const removeFromCart = (dessert) => {
+    const updatedCart = cart.filter((item) => item.name !== dessert.name)
+    setCart(updatedCart)
+    updateTotalPrice(updatedCart)
+    updateTotalQuantity(updatedCart)
+  }
+
+  
   return (
     <div className='whole-section'>
     <div class="products-section">
@@ -16,6 +58,7 @@ function App() {
       <Dessert
       key={dessert.name}
       dessert={dessert}
+      onAddToCart={addToCart}
       isInCart={cart.find((item) => item.name === dessert.name)}
     />
     ))}
@@ -23,7 +66,11 @@ function App() {
 </div>  
 
 <div className='cart-section'>
-  <Cart/>
+  <Cart
+  cart={cart}
+  onRemoveFromCart={removeFromCart}
+  totalPrice={totalPrice}
+  totalQuantity={totalQuantity}/>
 </div>
 </div>
     
